8999be790d92b7a4bf0e46a9f09ba1307bd560eb
Kirill Smetankin <smet_k@MacBook-Pro-Kirill.local>
2018-10-02 02:14:32 +0300

    working reading in str mas

--- project/src/main.c
+++ project/src/main.c
@@ -51,7 +51,7 @@ int add_item(struct mas_str* in_mas, struct buf* in_buf) {
     for (size_t i = 0; i < in_mas->size; i++) {
         copy_buf(&tmp[i], &in_mas->elem[i]);
 
-        printf("|%s|\n", tmp[i].str);
+        // printf("|%s|\n", tmp[i].str);
     }
 
     copy_buf(&tmp[in_mas->size], in_buf);
@@ -68,7 +68,7 @@ int str_input(struct buf* tmp_buf) {  // '\0' - 0, '\n' - 1
     // struct buf tmp_buf = {NULL, 0, 0};  // хранит только одну введенную строку
     char in_char;  // введенный символ
 
-    while ((in_char = getchar()) != EOF) {
+    while ((in_char = getchar()) != EOF) {  // В силайоне не работает поэтому там 'D'
         if (tmp_buf->size + 1 > tmp_buf->buf_size) {
             // size_t new_size = !tmp_buf.buf_size ? 1 : tmp_buf.buf_size * 2;
             size_t new_size = ((tmp_buf->buf_size * 2)>(16))?(tmp_buf->buf_size * 2):(16);
@@ -108,46 +108,9 @@ int main(void) {
         tmp_buf.size = 0;
         tmp_buf.buf_size = 0;
     }
-    // char in_char;  // введенный символ
-
-    // TODO(): бахнуть весь while в функцию
-    // while ((in_char = getchar()) != EOF) {
-    //     if (tmp_buf.size + 1 > tmp_buf.buf_size) {
-    //         // size_t new_size = !tmp_buf.buf_size ? 1 : tmp_buf.buf_size * 2;
-    //         size_t new_size = ((tmp_buf.buf_size * 2)>(16))?(tmp_buf.buf_size * 2):(16);
-
-    //         char* tmp = (char*)malloc((new_size + 1) * sizeof(char));
-    //         if (!tmp) {
-    //             // добавить освобождение памяти в буфере
-    //             printf("[error]\n");
-    //             return 0;
-    //         }
-    //         if (tmp_buf.str) {
-    //             strncpy(tmp, tmp_buf.str, (int)strlen(tmp_buf.str));
-    //             free(tmp_buf.str);
-    //         }
-
-    //         tmp_buf.str = tmp;
-    //         tmp_buf.buf_size = new_size;
-    //     }
-    //     tmp_buf.str[tmp_buf.size] = in_char;
-    //     tmp_buf.size++;
-
-    //     if (in_char == '\n') {
-    //         printf("out = %s", tmp_buf.str);
-
-    //         int jeppa = add_item(&all_str, &tmp_buf);
-    //         printf("add_item = %d\n", jeppa);
-
-    //         // printf("out1 = %s", all_str.elem[all_str.size].str);
-    //         // free(tmp_buf.str);
-    //     }
-    // }
-    // ---------------------------------------
-
 
     for (size_t i = 0; i < all_str.size; i++) {
-        printf("%zu\n", i);
+        printf("--[%zu]: |", i);
         printf("%s", all_str.elem[i].str);
     }
 
