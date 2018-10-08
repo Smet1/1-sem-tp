bb8e6b41f959a61aa8e8f74ac932098560dd9339
Kirill Smetankin <smet_k@MacBook-Pro-Kirill.local>
2018-10-08 11:14:28 +0300

    1-3 started

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -3,24 +3,58 @@
 // Найти количество таких пар индексов (i, j), что A[i] + B[j] = k. Время работы O(n + m).
 // n, m ≤ 100000. Указание.
 // Обходите массив B от конца к началу.
+
+// TEST
+//  in:
+//      4
+//      -5 0 3 18
+//      5
+//      -10 -2 4 7 12
+//      7
+//  out:
+//      3
+
 #include <iostream>
 
-int pair_count();
+int pair_count(int*, int, int*, int, int);
 
 int main(int argc, const char** argv) {
-    int n = 0, m = 0, k = 0;
+    int n = 0;  // size of A
     std::cin >> n;
     int *A = new int[n];
     for (size_t i = 0; i < n; i++) {
-        std::cin >> A[n];
+        std::cin >> A[i];
     }
-    for (size_t i = 0; i < n; i++) {
-        std::cout << A[n] << " ";
+
+    int m = 0;  // size of B
+    std::cin >> m;
+    int *B = new int[m];
+    for (size_t i = 0; i < m; i++) {
+        std::cin >> B[i];
     }
-    std::cout << std::endl;
 
-//    int res = pair_count();
+//    for (size_t i = 0; i < n; i++) {
+//        std::cout << A[i] << " ";
+//    }
+//    std::cout << std::endl;
+//
+//    for (size_t i = 0; i < m; i++) {
+//        std::cout << B[i] << " ";
+//    }
+//    std::cout << std::endl;
 
-    delete[] (A);
+    int k = 0;  // pair sum
+    std::cin >> k;
+    int res = pair_count(A, n, B, m, k);
+
+    std::cout << res << std::endl;
+
+    delete[] A;
+    delete[] B;
     return 0;
+}
+
+int pair_count(int* A, int n, int* B, int m, int k) {
+    int count = 0;
+    return count;
 }
