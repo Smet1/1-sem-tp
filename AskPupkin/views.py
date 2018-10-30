d712dfc51c411e21bce0d5bb271cc10a4db98887
Kirill Smetankin <smet_k@MacBook-Pro-Kirill.local>
2018-10-30 10:58:29 +0300

    added main and question views

--- AskPupkin/views.py
+++ AskPupkin/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
+def home_page(request):
+    return render(request, 'index.html')
+
+
+def question_id(request):
+    return render(request, 'question.html')
--- tp_1_sem/urls.py
+++ tp_1_sem/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from AskPupkin import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_page),
+    path('question', views.question_id),
 ]
