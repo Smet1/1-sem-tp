cd4b968074b8308a269f29758bef5a64383d2df2
smet_k <ski97@mail.ru>
2018-12-17 16:27:40 +0300

    3 test faild

--- project/src/3.cpp
+++ project/src/3.cpp
@@ -11,41 +11,46 @@
 #include <algorithm>
 #include <queue>
 
+template <class T>
 class Graph_weighted {
  public:
-    Graph_weighted() : graph_size(0), out_edges(0) {};
+    Graph_weighted() : graph_size(0), edges_count(0), out_edges(0) {};
     Graph_weighted(size_t graph_size, size_t edges_count) : graph_size(graph_size), edges_count(edges_count),
                                                    out_edges(edges_count) {};
     ~Graph_weighted() = default;
 
     size_t get_size() const { return graph_size; };
     size_t get_edges_count() const { return edges_count; };
-    void add_edge(int from, int to, int weight);
+    void add_edge(int from, int to, T weight);
 
-    std::vector<std::pair<int, int>> get_next_vertices(int vertex) const;
+    std::vector<std::pair<int, T>> get_next_vertices(int vertex) const;
     void print();
  private:
     size_t graph_size;
     size_t edges_count;
 
-    std::vector<std::vector<std::pair<int, int>>> out_edges;
+    std::vector<std::vector<std::pair<int, T>>> out_edges;
 };
 
-void Graph_weighted::add_edge(int from, int to, int weight) {
+template <class T>
+void Graph_weighted<T>::add_edge(int from, int to, T weight) {
     assert(from >= 0 && from < graph_size);
     assert(to >= 0 && to < graph_size);
+    assert(weight >= 0 && weight <= 10000);
 
     out_edges[from].emplace_back(to, weight);
     out_edges[to].emplace_back(from, weight);
 }
 
-std::vector<std::pair<int, int>> Graph_weighted::get_next_vertices(int vertex) const {
+template <class T>
+std::vector<std::pair<int, T>> Graph_weighted<T>::get_next_vertices(int vertex) const {
     assert(vertex >= 0 && vertex < graph_size);
 
     return out_edges[vertex];
 }
 
-void Graph_weighted::print() {
+template <class T>
+void Graph_weighted<T>::print() {
     for (int i = 0; i < graph_size; ++i) {
         std::cout << i << ": ";
         for (auto out_target : out_edges[i]) {
@@ -70,103 +75,80 @@ void print_deque(std::deque<int> &queue) {
     std::cout << "===========" << std::endl;
 }
 
+template <class T>
+struct Edge {
+    Edge() : vert_from(0), vert_to(0), weight(0) {};
+    Edge(size_t from, size_t to, T weight) : vert_from(from), vert_to(to), weight(weight) {};
+    Edge(size_t from, size_t to) : vert_from(from), vert_to(to) {};
+
+    size_t vert_from;
+    size_t vert_to;
+    T weight;
+};
+
+template <class T>
+void print_deque(std::deque<Edge<T>> &queue) {
+    std::cout << "===========\ndeque (" << queue.size() << ")\n";
+    for (size_t i = 0; i < queue.size(); i++)
+        std::cout << "[" << i << "] = (f = " << queue[i].vert_from << ", t = " << queue[i].vert_to << ", w = " << queue[i].weight << ")\n";
+    std::cout << "===========" << std::endl;
+}
+
 // TODO(): восстанавливать путь, а не брать макс в массиве (непрвильный ответ)
 // TODO(): при отсутсвии маршрута не работает (скорее всего проблема в пред пункте)
 
-//size_t dijkstra(const Graph_weighted *graph, const int &begin, const int &end) {
-//    size_t size_graph = graph->get_size();
-//    std::deque<std::pair<int, int>> queue;
-//    std::deque<std::pair<int, int>> tmp_queue;  // чтобы контролировать номер волны
-//    std::set<int> visited;
-//
-//    std::vector<int> vertices_weights(size_graph, static_cast<const int &>(graph->get_edges_count() + 1));
-//    std::vector<std::vector<int>> verts_from(size_graph);  // вершины откуда пришли в текущую по минимальному весу
-//    std::vector<unsigned short> temp = graph->get_next_vertices(begin);
-//
-//    for (auto i : temp) {
-//        //        std::cout << i << " ";
-//        queue.emplace_back(begin, i);
-//    }
-//    //    std::cout << std::endl;
-//    //    print_deque(queue);
-//
-//    vertices_weights[begin] = 0;
-//
-//    int tmp_from = 0;
-//    int tmp_to = 0;
-//    int wave_num = 1;
-//    while (!queue.empty()) {
-//        //        std::cout << "wave_num = " << wave_num << std::endl;
-//        //        print_deque(queue);
-//
-//        tmp_from = queue.front().first;
-//        tmp_to = queue.front().second;
-//        queue.pop_front();
-//
-//        //        if (visited.find(tmp_to) != visited.end()) {
-//        //            continue;
-//        //        } else {
-//        //            visited.insert(tmp_to);
-//        //        }
-//
-//        if (vertices_weights[tmp_to] > wave_num) {
-//            vertices_weights[tmp_to] = wave_num;
-//            verts_from[tmp_to].clear();
-//            verts_from[tmp_to].push_back(tmp_from);
-//        } else if (vertices_weights[tmp_to] == wave_num) {
-//            verts_from[tmp_to].push_back(tmp_from);
-//        }
-//
-//        if (visited.find(tmp_to) == visited.end()) {
-//            for (auto i : graph->get_next_vertices(tmp_to)) {
-//                tmp_queue.emplace_back(tmp_to, i);
-//            }
-//            visited.insert(tmp_to);
-//        }
-//
-//        if (queue.empty()) {
-//            queue = tmp_queue;
-//            tmp_queue.clear();
-//            ++wave_num;
-//        }
-//    }
-//
-//    //    std::cout << std::endl;
-//
-//    size_t max = 0;
-//    //    for (auto i : verts_from) {
-//    //        max = std::max(max, i.size());
-//    //    }
-//
-//    if (verts_from[end].empty())
-//        return 0;
-//    else {
-//        std::deque<int> tmp;
-//        std::deque<int> routes;
-//        for (auto i : verts_from[end])
-//            routes.push_back(i);
-//
-//        max = routes.size();
-//
-//        while (!routes.empty()) {
-//            for (auto j : routes) {
-//                for (auto k : verts_from[j])
-//                    tmp.push_back(k);
-//            }
-//
-//            //            print_deque(routes);
-//            //            print_deque(tmp);
-//
-//            routes = tmp;
-//            tmp.clear();
-//
-//            max = (max < routes.size()) ? routes.size() : max;
-//        }
-//
-//    }
-//
-//    return max;
-//}
+template <class T>
+size_t dijkstra(const Graph_weighted<T> *graph, const int &begin, const int &end) {
+    size_t graph_size = graph->get_size();
+    std::deque<Edge<T>> deque;
+    std::deque<Edge<T>> tmp_deque;
+
+    std::set<int> visited;
+
+//    std::vector<T> vertices_weights(graph_size, graph->get_edges_count() + 1);
+    std::vector<T> vertices_weights(graph_size, std::numeric_limits<T>::max());
+    vertices_weights[begin] = 0;
+
+    for (auto i : graph->get_next_vertices(begin))
+        deque.emplace_back(begin, i.first, i.second);
+
+//    print_deque(deque);
+
+    int tmp_from(0);
+    int tmp_to(0);
+    T tmp_weight(0);
+    int wave_num(1);
+
+    while (!deque.empty()) {
+//        std::cout << "wave_num = " << wave_num << std::endl;
+//        print_deque(deque);
+
+        tmp_from = deque.front().vert_from;
+        tmp_to = deque.front().vert_to;
+        tmp_weight = deque.front().weight;
+        deque.pop_front();
+
+        if (vertices_weights[tmp_to] > tmp_weight) {
+            vertices_weights[tmp_to] = tmp_weight + vertices_weights[tmp_from];
+            // можно еще хранить точки откуда, но в данной задаче излишне
+        }
+
+        if (visited.find(tmp_to) == visited.end()) {
+            for (auto i : graph->get_next_vertices(tmp_to)) {
+                tmp_deque.emplace_back(tmp_to, i.first, i.second);
+            }
+            visited.insert(tmp_to);
+        }
+
+        if (deque.empty()) {
+            ++wave_num;
+            deque = tmp_deque;
+            tmp_deque.clear();
+        }
+    }
+
+    return vertices_weights[end];
+}
 
 int main() {
     int vert_count(0);
@@ -175,12 +157,12 @@ int main() {
     int from(0), to(0), weight(0);
 
     std::cin >> vert_count;
-    assert(vert_count >= 0 && vert_count <= 50000);
+    assert(vert_count >= 0 && vert_count <= 10000);
 
     std::cin >> edges_count;
-    assert(edges_count >= 0 && edges_count <= 200000);
+    assert(edges_count >= 0 && edges_count <= 250000);
 
-    Graph_weighted graph(static_cast<size_t>(vert_count), static_cast<size_t>(edges_count));
+    Graph_weighted<int> graph(static_cast<size_t>(vert_count), static_cast<size_t>(edges_count));
 
     for (size_t i = 0; i < edges_count; ++i) {
         std::cin >> from;  // asserting vals in add_edge()
@@ -188,6 +170,7 @@ int main() {
         std::cin >> weight;
 
         graph.add_edge(from, to, weight);
+//        graph.add_edge(to, from, weight);
     }
 
     std::cin >> vertex_from;
@@ -196,11 +179,11 @@ int main() {
     std::cin >> vertex_to;
     assert(vertex_to >= 0 && vertex_to <= vert_count);
 
-        graph.print();
+//    graph.print();
 
     // run dijkstra
-//    size_t routes_count = dijkstra(&graph, vertex_from, vertex_to);
-//    std::cout << routes_count;
+    size_t weight_route = dijkstra<int>(&graph, vertex_from, vertex_to);
+    std::cout << weight_route;
 
     return 0;
 }
