03caf91be6f752c111cac75a3768b3041b3579b7
smet_k <ski97@mail.ru>
2018-12-22 01:13:17 +0300

    rewrited dijkstra

--- project/src/3.cpp
+++ project/src/3.cpp
@@ -11,12 +11,12 @@
 #include <algorithm>
 #include <queue>
 
-template <class T>
+template<class T>
 class Graph_weighted {
  public:
     Graph_weighted() : graph_size(0), edges_count(0), out_edges(0) {};
     Graph_weighted(size_t graph_size, size_t edges_count) : graph_size(graph_size), edges_count(edges_count),
-                                                   out_edges(edges_count) {};
+                                                            out_edges(edges_count) {};
     ~Graph_weighted() = default;
 
     size_t get_size() const { return graph_size; };
@@ -32,24 +32,24 @@ class Graph_weighted {
     std::vector<std::vector<std::pair<unsigned short, T>>> out_edges;
 };
 
-template <class T>
+template<class T>
 void Graph_weighted<T>::add_edge(unsigned short from, unsigned short to, T weight) {
-//    assert(from >= 0 && from < graph_size);
-//    assert(to >= 0 && to < graph_size);
-//    assert(weight >= 0 && weight <= 10000);
+    //    assert(from >= 0 && from < graph_size);
+    //    assert(to >= 0 && to < graph_size);
+    //    assert(weight >= 0 && weight <= 10000);
 
     out_edges[from].emplace_back(to, weight);
     out_edges[to].emplace_back(from, weight);
 }
 
-template <class T>
+template<class T>
 std::vector<std::pair<unsigned short, T>> Graph_weighted<T>::get_next_vertices(unsigned short vertex) const {
-//    assert(vertex >= 0 && vertex < graph_size);
+    //    assert(vertex >= 0 && vertex < graph_size);
 
     return out_edges[vertex];
 }
 
-template <class T>
+template<class T>
 void Graph_weighted<T>::print() {
     for (int i = 0; i < graph_size; ++i) {
         std::cout << i << ": ";
@@ -75,12 +75,14 @@ void print_deque(std::deque<int> &queue) {
     std::cout << "===========" << std::endl;
 }
 
-template <class T>
+template<class T>
 struct Edge {
     Edge() : vert_from(0), vert_to(0), weight(0) {};
-    Edge(unsigned short from, unsigned short to, T weight) : vert_from(from), vert_to(to), weight(weight), priority(weight) {};
+    Edge(unsigned short from, unsigned short to, T weight)
+        : vert_from(from), vert_to(to), weight(weight), priority(weight) {};
     Edge(unsigned short from, unsigned short to) : vert_from(from), vert_to(to), weight(0), priority(0) {};
-    Edge(unsigned short from, unsigned short to, T weight, unsigned short priority) : vert_from(from), vert_to(to), weight(weight), priority(priority) {};
+    Edge(unsigned short from, unsigned short to, T weight, unsigned short priority) :
+        vert_from(from), vert_to(to), weight(weight), priority(priority) {};
 
     unsigned short vert_from;
     unsigned short vert_to;
@@ -88,81 +90,89 @@ struct Edge {
     unsigned short priority;
 };
 
-template <class T>
-bool operator <(const Edge<T> &x, const Edge<T> &y) {
+template<class T>
+bool operator<(const Edge<T> &x, const Edge<T> &y) {
     return (x.priority != y.priority) ? x.priority <= y.priority : x.weight <= y.weight;
 }
 
-template <class T>
-void print_deque(std::deque<Edge<T>> &queue) {
+template<class T>
+void print_deque(std::set<std::pair<T, unsigned short >> &queue) {
     std::cout << "===========\ndeque (" << queue.size() << ")\n";
-    for (size_t i = 0; i < queue.size(); i++)
-        std::cout << "[" << i << "] = (f = " << queue[i].vert_from << ", t = " << queue[i].vert_to << ", w = "
-                  << queue[i].weight << ", p = " << queue[i].priority << ")\n";
+    int i = 0;
+    for (auto j : queue)
+        std::cout << "[" << i++ << "] = (t = " << j.second << ", w = " << j.first << ")\n";
     std::cout << "===========" << std::endl;
 }
 
-template <class T>
+template<class T>
 void print_deque(std::set<Edge<T>> &queue) {
     std::cout << "===========\ndeque (" << queue.size() << ")\n";
     int i = 0;
     for (auto j : queue)
         std::cout << "[" << i++ << "] = (f = " << j.vert_from << ", t = " << j.vert_to << ", w = " << j.weight
-        << ", p = " << j.priority << ")\n";
+                  << ", p = " << j.priority << ")\n";
     std::cout << "===========" << std::endl;
 }
 
-template <class T>
+template<class T>
 size_t dijkstra(const Graph_weighted<T> *graph, const int &begin, const int &end) {
-    size_t graph_size = graph->get_size();
-//    std::set<Edge<T>> deque;
-    std::set<Edge<T>> tmp_deque;
+    //    size_t graph_size = graph->get_size();
+    //    std::set<Edge<T>> tmp_deque;
+    //
+    //    std::vector<T> vertices_weights(graph_size, std::numeric_limits<T>::max());
+    //    vertices_weights[begin] = 0;
+    //
+    //    for (auto i : graph->get_next_vertices(begin))
+    //        tmp_deque.emplace(begin, i.first, i.second);
+    //
+    //    size_t tmp_from(0);
+    //    size_t tmp_to(0);
+    //    T tmp_weight(0);
+    //
+    //    while (!tmp_deque.empty()) {
+    //        tmp_from = tmp_deque.begin()->vert_from;
+    //        tmp_to = tmp_deque.begin()->vert_to;
+    //        tmp_weight = tmp_deque.begin()->weight;
+    //        tmp_deque.erase(tmp_deque.begin());
+    //
+    //        if (vertices_weights[tmp_to] > tmp_weight + vertices_weights[tmp_from]) {
+    //            vertices_weights[tmp_to] = tmp_weight + vertices_weights[tmp_from];
+    //            // можно еще хранить точки откуда, но в данной задаче излишне
+    //            for (auto i : graph->get_next_vertices(tmp_to)) {
+    //                if (i.first != tmp_from)
+    //                    tmp_deque.emplace(tmp_to, i.first, i.second, i.second + vertices_weights[tmp_to]);
+    //                    // + vertices_weights[tmp_from] чтобы правильно работал сет
+    //            }
+    //        }
+    //    }
 
-    std::set<unsigned short> visited;
+    size_t graph_size = graph->get_size();
+    std::set<std::pair<T, unsigned short>> tmp_deque;  // иначе очередь не работает, только с парами
 
     std::vector<T> vertices_weights(graph_size, std::numeric_limits<T>::max());
+    unsigned short tmp_from(0);
+    unsigned short tmp_to(0);
+    T tmp_weight(0);
     vertices_weights[begin] = 0;
 
-    for (auto i : graph->get_next_vertices(begin))
-        tmp_deque.emplace(begin, i.first, i.second);
-
-    size_t tmp_from(0);
-    size_t tmp_to(0);
-    T tmp_weight(0);
+    tmp_deque.emplace(0, begin);
 
     while (!tmp_deque.empty()) {
-        tmp_from = tmp_deque.begin()->vert_from;
-        tmp_to = tmp_deque.begin()->vert_to;
-        tmp_weight = tmp_deque.begin()->weight;
+        print_deque(tmp_deque);
+        tmp_from = tmp_deque.begin()->second;
+        //        tmp_weight = tmp_deque.begin()->first;
         tmp_deque.erase(tmp_deque.begin());
 
-        if (vertices_weights[tmp_to] > tmp_weight + vertices_weights[tmp_from]) {
-            vertices_weights[tmp_to] = tmp_weight + vertices_weights[tmp_from];
-            // можно еще хранить точки откуда, но в данной задаче излишне
-            if (visited.find(tmp_to) != visited.end()) {
-                for (auto i : graph->get_next_vertices(tmp_to)) {
-                    if (i.first != tmp_from)
-                        tmp_deque.emplace(tmp_to, i.first, i.second, i.second + vertices_weights[tmp_to]);
-                        // + vertices_weights[tmp_from] чтобы правильно работал сет
-                }
+        for (auto vert : graph->get_next_vertices(tmp_from)) {
+            if (vertices_weights[vert.first] > vert.second + vertices_weights[tmp_from]) {
+                tmp_deque.erase(std::make_pair(vertices_weights[tmp_from], vert.first));
+
+                vertices_weights[vert.first] = vert.second + vertices_weights[tmp_from];
+
+                tmp_deque.insert(std::make_pair(vertices_weights[vert.first], vert.first));
             }
         }
-
-        if (visited.find(tmp_to) == visited.end()) {
-            for (auto i : graph->get_next_vertices(tmp_to)) {
-                if (i.first != tmp_from)
-                    tmp_deque.emplace(tmp_to, i.first, i.second, i.second + vertices_weights[tmp_to]);
-                    // + vertices_weights[tmp_from] чтобы правильно работал сет
-            }
-            visited.insert(tmp_to);
-        }
-
-//        if (deque.empty()) {
-//            print_deque(tmp_deque);
-////            deque = std::move(tmp_deque);
-//        }
     }
-
 
     return vertices_weights[end];
 }
@@ -174,10 +184,10 @@ int main() {
     int from(0), to(0), weight(0);
 
     std::cin >> vert_count;
-//    assert(vert_count >= 0 && vert_count <= 10000);
+    //    assert(vert_count >= 0 && vert_count <= 10000);
 
     std::cin >> edges_count;
-//    assert(edges_count >= 0 && edges_count <= 250000);
+    //    assert(edges_count >= 0 && edges_count <= 250000);
 
     Graph_weighted<unsigned short> graph(static_cast<size_t>(vert_count), static_cast<size_t>(edges_count));
 
@@ -187,16 +197,16 @@ int main() {
         std::cin >> weight;
 
         graph.add_edge(from, to, weight);
-//        graph.add_edge(to, from, weight);
+        //        graph.add_edge(to, from, weight);
     }
 
     std::cin >> vertex_from;
-//    assert(vertex_from >= 0 && vertex_from <= vert_count);
+    //    assert(vertex_from >= 0 && vertex_from <= vert_count);
 
     std::cin >> vertex_to;
-//    assert(vertex_to >= 0 && vertex_to <= vert_count);
+    //    assert(vertex_to >= 0 && vertex_to <= vert_count);
 
-//    graph.print();
+    //    graph.print();
 
     // run dijkstra
     size_t weight_route = dijkstra<unsigned short>(&graph, vertex_from, vertex_to);
@@ -204,3 +214,19 @@ int main() {
 
     return 0;
 }
+
+/*
+7 11
+0 3 1
+0 4 2
+1 2 7
+1 3 2
+1 4 3
+1 5 3
+2 5 3
+3 4 4
+3 5 6
+5 6 1
+6 1 1
+2 0
+ */
